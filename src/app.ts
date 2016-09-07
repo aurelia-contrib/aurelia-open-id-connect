b30b60cd2e94ea4deeb399fd088abbd0b9409c92
Shaun Luttin <admin@shaunluttin.com>
2016-09-07 14:30:49 -0700

    Add an authorization step to the pipeline

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,70 @@
 import { autoinject } from "aurelia-framework";
-import { RouterConfiguration, Router } from "aurelia-router";
+import { Redirect, NavigationInstruction, RouterConfiguration, Router } from "aurelia-router";
 import { OpenId } from "./open-id/open-id";
 
 @autoinject
 export class App {
 
-    public openId: OpenId;
+    private router: Router;
 
-    constructor(openId: OpenId) {
+    constructor(private openId: OpenId) {
         this.openId = openId;
     }
 
-    public configureRouter(routerConfiguration: RouterConfiguration) {
+    public configureRouter(routerConfiguration: RouterConfiguration, router: Router) {
 
         // switch from hash (#) to slash (/) navigation
         routerConfiguration.options.pushState = true;
 
+        routerConfiguration.title = "Demo";
+
+        routerConfiguration.addPipelineStep('authorize', AuthorizeStep);
+
         // configure routes
         routerConfiguration.map([
-            { moduleId: "home", route: [""] },
+            // OpenId
             {
-                route: 'login', name: 'login', nav: false, navigationStrategy: () => this.openId.Login()
+                route: 'login', name: 'login', nav: false, navigationStrategy: () => this.openId.Login(),
+                settings: { roles: ['anonymous'] }
             },
             {
-                route: 'logout', name: 'logout', nav: false, navigationStrategy: () => this.openId.Logout()
+                route: 'logout', name: 'logout', nav: false, navigationStrategy: () => this.openId.Logout(),
+                settings: { roles: ['authorized'] }
+            },
+            // App
+            {
+                route: ['', 'home'], name: 'home', nav: true, moduleId: 'home', title: 'home',
+                settings: { roles: ['everyone'] }
             },
             {
-                route: 'profile', name: 'profile', nav: false, moduleId: 'profile'
-            }
+                route: 'profile', name: 'profile', nav: true, moduleId: 'open-id-profile', title: 'profile',
+                settings: { roles: ['authorized'] }
+            },
         ]);
 
         this.openId.Configure(routerConfiguration);
+        this.router = router;
+    }
+}
+
+@autoinject
+class AuthorizeStep {
+
+    constructor(private openId: OpenId) {
+        this.openId = openId;
+    }
+
+    run(navigationInstruction: NavigationInstruction, next: any): Promise<any> {
+
+        // check authorized
+        if (navigationInstruction.getAllInstructions().some(i => i.config.settings.roles.indexOf('authorized') !== -1)) {
+            return this.openId.UserManager.getUser().then((user) => {
+                let isAuthorized: boolean = user !== null;
+                if (!isAuthorized) {
+                    return next.cancel(new Redirect("login"));
+                }
+            });
+        }
+        return next();
     }
 }
