cc07690fc03f35c1bc390b8d24ebf7ded0047b55
Shaun Luttin <admin@shaunluttin.com>
2016-09-08 16:34:02 -0700

    Compile and include html files

--- dist/amd/es6/app.d.ts
+++
@@ -1,9 +0,0 @@
-import { RouterConfiguration, Router } from "aurelia-router";
-import { OpenIdConnect } from "./open-id-connect/index";
-export declare class App {
-    private openIdConnect;
-    private router;
-    private user;
-    constructor(openIdConnect: OpenIdConnect);
-    configureRouter(routerConfiguration: RouterConfiguration, router: Router): void;
-}
