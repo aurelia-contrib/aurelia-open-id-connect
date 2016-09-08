d859a6515ffee7df22d0cf8ccfe4274874ddd4e8
Shaun Luttin <admin@shaunluttin.com>
2016-09-07 19:10:02 -0700

    Use FrameworkConfiguration in a plugin not Aurelia

--- src/open-id/open-id.ts
+++ src/open-id/open-id.ts
@@ -1,20 +1,22 @@
-import { autoinject, Aurelia } from "aurelia-framework";
+import { autoinject, FrameworkConfiguration } from "aurelia-framework";
 import { RouterConfiguration } from "aurelia-router";
 import { User, UserManager, UserManagerSettings } from "oidc-client";
 import { OpenIdRouterConfigurationService } from "./open-id-router-configuration-service";
 import { OpenIdConfiguration } from "./open-id-configuration";
 import { OpenIdLogger } from "./open-id-logger";
 
-function configure(aurelia: Aurelia, callback: Function) {
+function configure(config: FrameworkConfiguration, callback: Function) {
 
-    let logger: OpenIdLogger = aurelia.container.get(OpenIdLogger);
+    let logger: OpenIdLogger = config.container.get(OpenIdLogger);
+    
+    // config.globalResources('./foo');
 
     callback(function (oidcConfig: OpenIdConfiguration) {
         logger.Debug("Configuring the OpenId Connect Client");
 
         let userManagerSettings = oidcConfig.UserManagerSettings;
-        aurelia.container.registerInstance(UserManager, new UserManager(userManagerSettings));
-        aurelia.container.registerInstance(OpenIdConfiguration, oidcConfig);
+        config.container.registerInstance(UserManager, new UserManager(userManagerSettings));
+        config.container.registerInstance(OpenIdConfiguration, oidcConfig);
     });
 }
 
