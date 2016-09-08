f79483bdd86d2b003913d3982198f26b81ecc532
Shaun Luttin <admin@shaunluttin.com>
2016-09-08 16:34:02 -0700

    Compile and include html files

--- dist/amd/es6/open-id-connect-configuration.d.ts
+++
@@ -1,3 +0,0 @@
-import { OpenIdConnectConfiguration } from "./open-id-connect/index";
-declare const oidcConfig: OpenIdConnectConfiguration;
-export default oidcConfig;
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="custom_typings/workarounds.d.ts" />
+/// <reference path="custom_typings/jwt-decode.d.ts" />
 /// <reference path="custom_typings/oidc-client-js.d.ts" />
 /// <reference path="typings/index.d.ts" />
 
