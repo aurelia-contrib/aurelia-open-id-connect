eb9a2227946a22c67a643b98cbf2d63da3a61e55
Shaun Luttin <admin@shaunluttin.com>
2016-11-22 19:16:29 -0800

    Remove commented out code.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 
-// export * from "./open-id-connect-authorize-step";
-// export * from "./open-id-connect-routing";
-// export * from "./open-id-connect-logger";
-
 // by convention, we must call this `configure`
 import configure from "./plugin";
 import OpenIdConnect from "./open-id-connect";
