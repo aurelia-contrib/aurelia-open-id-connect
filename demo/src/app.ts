a1ffa3a8a3bd51f109f7f07eb199042478e21d84
Shaun Luttin <admin@shaunluttin.com>
2017-01-16 15:14:44 -0800

    Remove experimental features from demo

--- demo/src/app.ts
+++ demo/src/app.ts
@@ -6,44 +6,32 @@ import { OpenIdConnect, OpenIdConnectRoles } from "aurelia-open-id-connect";
 @autoinject
 export class App {
 
-    private router: Router;
-    private user: User;
+  private router: Router;
+  private user: User;
 
-    constructor(private openIdConnect: OpenIdConnect) {
-        this.openIdConnect.logger.enableOidcClientLogging(Log.INFO);
+  constructor(private openIdConnect: OpenIdConnect) {
+    this.openIdConnect.logger.enableOidcClientLogging(Log.INFO);
 
-        this.openIdConnect.userManager.getUser().then((user) => {
-            this.user = user;
-        });
-    }
+    this.openIdConnect.userManager.getUser().then((user) => {
+      this.user = user;
+    });
+  }
 
-    public configureRouter(routerConfiguration: RouterConfiguration, router: Router) {
+  public configureRouter(routerConfiguration: RouterConfiguration, router: Router) {
 
-        // switch from hash (#) to slash (/) navigation
-        routerConfiguration.options.pushState = true;
+    // switch from hash (#) to slash (/) navigation
+    routerConfiguration.options.pushState = true;
+    routerConfiguration.title = "OpenID Connect Implicit Flow Demo";
 
-        routerConfiguration.title = "OpenID Connect Implicit Flow Demo";
+    // configure routes
+    routerConfiguration.map([
+      {
+        moduleId: "home", name: "home", nav: true, route: [""],
+        settings: { roles: [OpenIdConnectRoles.Everyone] }, title: "home",
+      },
+    ]);
 
-        // configure routes
-        routerConfiguration.map([
-            {
-                moduleId: "home", name: "home", nav: true, route: [""],
-                settings: { roles: [OpenIdConnectRoles.Everyone] }, title: "home",
-            },
-            // todo: determine why profile is not showing up in the demo
-            {
-                moduleId: "user-profile", name: "profile", nav: true, route: "profile",
-                settings: { roles: [OpenIdConnectRoles.Authorized] }, title: "profile",
-            },
-            // todo: Add an admin module and admin role for the demo
-            // todo: because this route is currently a non-functional stub.
-            {
-                moduleId: "admin", name: "admin", nav: true, route: "admin",
-                settings: { roles: [OpenIdConnectRoles.Administrator] }, title: "admin",
-            },
-        ]);
-
-        this.openIdConnect.configure(routerConfiguration);
-        this.router = router;
-    }
+    this.openIdConnect.configure(routerConfiguration);
+    this.router = router;
+  }
 }
