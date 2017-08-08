3fe638f3b3058360461a75adf859e8fe865473c0
Shaun Luttin <admin@shaunluttin.com>
2017-08-08 15:23:24 -0700

    Delete demo directory

--- demo/src/resources/index.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    function configure(config) {
-    }
-    exports.configure = configure;
-});
-//# sourceMappingURL=index.js.map
