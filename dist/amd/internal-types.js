d6f06d9b6ff93f0d9bfd0a78957bf5a28a5a61f8
Shaun Luttin <admin@shaunluttin.com>
2017-11-23 18:18:43 -0800

    Package the internal-types

---
+++ dist/amd/internal-types.js
@@ -0,0 +1,5 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
+//# sourceMappingURL=internal-types.js.map
