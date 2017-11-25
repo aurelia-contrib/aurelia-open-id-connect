179ee711c063f543006a099e2c09b117d5bded69
Shaun Luttin <admin@shaunluttin.com>
2017-11-24 17:12:00 -0800

    Start unit-test for logging

--- src/open-id-connect-logger.ts
+++ src/open-id-connect-logger.ts
@@ -2,19 +2,50 @@ import { Log } from "oidc-client";
 
 export default class OpenIdConnectLogger {
 
-    private level: number = Log.NONE;
+    // tslint:disable-next-line:variable-name
+    private _level: number = Log.NONE;
+
+    public get level() {
+        return this._level;
+    }
 
     constructor(level: number) {
         this.setLogLevel(level);
     }
 
-    // TODO: Support more log levels.
-    public debug(message: string) {
-        if (this.level === Log.ERROR) {
+    // +++++++++++++++++++++++++++++++++++++++++++++
+    // TODO: Use currying or function overloading to encapsulate shared logic.
+
+    public debug(msg: string) {
+        if (this.level >= Log.DEBUG) {
             /* tslint:disable no-console */
-            console.debug(`DEBUG [OpenIdConnect] ${message}`);
+            console.debug(`DEBUG [OpenIdConnect] ${msg}`);
         }
     }
+
+    public info(msg: string) {
+        if (this.level >= Log.INFO) {
+            /* tslint:disable no-console */
+            console.info(`INFO [OpenIdConnect] ${msg}`);
+        }
+    }
+
+    public warn(msg: string) {
+        if (this.level >= Log.WARN) {
+            /* tslint:disable no-console */
+            console.warn(`WARN [OpenIdConnect] ${msg}`);
+        }
+    }
+
+    public error(msg: string) {
+        if (this.level >= Log.ERROR) {
+            /* tslint:disable no-console */
+            console.error(`ERROR [OpenIdConnect] ${msg}`);
+        }
+    }
+
+    // END TODO
+    // +++++++++++++++++++++++++++++++++++++++++++++++
 
     /**
      * Set the log level for both the aurelia-open-id-connect logger
@@ -22,21 +53,24 @@ export default class OpenIdConnectLogger {
      */
     private setLogLevel(level: number) {
 
+        // Levels are ordered from most to least verbose.
+        // The DEBUG level includes all the log levels.
         const validOidcClientLevels: number[] = [
-            Log.INFO,
-            Log.WARN,
-            Log.ERROR,
-            Log.NONE,
+            Log.DEBUG, // 4
+            Log.INFO, // 3
+            Log.WARN, // 2
+            Log.ERROR, // 1
+            Log.NONE, // 0
         ];
 
-        if (validOidcClientLevels.indexOf(level) >= 0) {
-            this.level = level;
-            Log.level = level;
-            Log.logger = console;
-        } else {
+        if (!validOidcClientLevels.includes(level)) {
             const levels: string = validOidcClientLevels.join(", ");
             const msg: string = `The log level must be one of ${levels}`;
-            this.debug(msg);
+            throw new RangeError(msg);
         }
+
+        this._level = level;
+        Log.level = level;
+        Log.logger = console;
     }
 }
