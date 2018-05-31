b776661b436e6de7caa1719a2ebc8278ac3df76d
Andrew Camilleri <evilkukka@gmail.com>
2018-05-31 13:13:00 +0200

    allow args to be passed to internal oidc-js

--- src/open-id-connect.ts
+++ src/open-id-connect.ts
@@ -25,13 +25,11 @@ export default class OpenIdConnect {
     this.openIdConnectRouting.configureRouter(routerConfiguration);
   }
 
-  public async login(): Promise<void> {
-    const args: any = {};
+  public async login(args: any = {}): Promise<void> {
     await this.userManager.signinRedirect(args);
   }
 
-  public async logout(): Promise<void> {
-    const args: any = {};
+  public async logout(args: any = {}): Promise<void> {
     try {
       await this.userManager.signoutRedirect(args);
     } catch (err) {
@@ -45,8 +43,7 @@ export default class OpenIdConnect {
     }
   }
 
-  public loginSilent(): Promise<User> {
-    const args: any = {};
+  public loginSilent(args: any = {}): Promise<User> {
     return this.userManager.signinSilent(args);
   }
 
